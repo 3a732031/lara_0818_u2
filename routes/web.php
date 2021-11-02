0d5ee51d6486e31566885087c54e2e92bc63bc62
jing yuan chen <3a732031@gm.student.ncut.edu.tw>
2021-11-03 06:54:18 +0800

    建立HellController、在view中新增hello及設定route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,3 +24,4 @@ Route::get('home', function () {
     return view('home.index');
 });*/
 Route::get('home', [HomeController::class,'index'])->name('home.index');
+Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
